901deea208bb84174e6498020629e21d4642cada
Bai404 <superbrn@163.com>
2019-02-09 00:54:28 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class='icons'>
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-con" :src="item.imgUrl">
+          </div>
+          <p class="icon-keywords">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/de711920c71e38287d9f95f7910aa1aa.png',
+        desc: '精品小团'
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      },
+      {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+        desc: '踏青赏花'
+      },
+      {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+        desc: '踏青赏花'
+      },
+      {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+        desc: '全部玩乐'
+      },
+      {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+        desc: '踏青赏花'
+      },
+      {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+        desc: '踏青赏花'
+      },
+      {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+        desc: '全部玩乐'
+      }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+  .icons >>> .swiper-slide-active
+    height: 0
+    padding-bottom: 50%
+  .icons
+    overflow: hidden
+    .icon
+      position: relative
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      text-align: center
+      .icon-img
+        position: absolute
+        left: 0
+        right: 0
+        top: .2rem
+        bottom: .4rem
+        width: 1.1rem;
+        height: 1.1rem;
+        margin: 0 auto
+        display: block
+        .icon-img-con
+          height:100%
+      .icon-keywords
+        width: 100%
+        position: absolute
+        bottom: .25rem
+        height: .4rem
+        line-height: .4rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
+</style>
