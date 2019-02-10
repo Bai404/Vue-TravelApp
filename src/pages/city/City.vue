d516e22f451f6d8741e030dc3e695f7e140fc8b6
Bai404 <superbrn@163.com>
2019-02-10 10:36:26 +0800

    add part of city

---
+++ src/pages/city/City.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    height: .68rem;
+    line-height: .68rem;
+    border-bottom: 0;
+    position: relative;
+    background: #00bcd4;
+</style>
